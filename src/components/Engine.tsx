b8c478d4cba0de3585748363c7bb5d8efb50376d
Anthony Calo <63485111+AnthonyCalo@users.noreply.github.com>
2023-10-31 14:17:04 -0400

    Update Engine.tsx

--- src/components/Engine.tsx
+++ src/components/Engine.tsx
@@ -49,7 +49,7 @@ const EngineBut = (props)=>{
     const getBest=(movesListicle: string)=>{
         if(movesListicle){
             var bestMove ="" 
-            fetch("https://www.calo-chess-backend.herokuapp.com/", {
+            fetch("calo-chess-backend.herokuapp.com/", {
                     method: "POST",
                     headers: {'Content-Type': 'application/json',
                                 'Accept': 'application/json'},
